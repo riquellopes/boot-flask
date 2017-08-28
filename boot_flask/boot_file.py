88676a01c89a16d7e03389a5cb79f1d25ed974cd
Henrique <everaldo.lopes@concrete.com.br>
2017-08-28 09:36:24 -0300

    starting a new option to bootflask. APi mode.

--- boot_flask/boot_file.py
+++ boot_flask/boot_file.py
@@ -3,6 +3,7 @@ from inspect import getdoc
 from boot_flask_base import BootFlaskBase, BootFlaskException
 
 
+# WEB
 class BootFlaskFile(BootFlaskBase):
     __name__ = ""
     __doc__ = """"""
@@ -84,3 +85,70 @@ class BootFlaskRequiriments(BootFlaskFile):
     def write(self):
         self.__doc__ = os.popen("pip freeze").read()
         super(BootFlaskRequiriments, self).write()
+
+
+# API
+class BootFlasApi(BootFlaskFile):
+    __name__ = 'api.py'
+    __doc__ = """
+        from flask_restful import Api
+        from app import app as application
+        from app.db import db
+        from app.resources import SampleResource
+
+        def setup_app():
+            db.init_app(application)
+
+            api = Api(application, prefix="/v1")
+            api.add_resource(SampleResource, "/sample/", methods=['GET'])
+
+            return application
+
+
+        app = setup_app()
+    """
+
+
+class BootFlaskSampleResource(BootFlaskFile):
+    __name__ = "resources.py"
+    __doc__ = """
+        import http
+        from flask import jsonify, make_response
+        from flask_restful import Resource
+        from webargs.flaskparser import use_args
+        from app.models import SampleModel
+        from app.schemas import SampleSchema
+        from app.db import db
+
+
+        class SampleResource(Resource):
+
+            def get(self):
+                schema = SampleSchema(many=True)
+                data = schema.dump(SampleModel.query.all())
+                return make_response(jsonify(results=data.data))
+
+            @use_args(SampleSchema(strict=True, only=("name")), locations=("json", ))
+            def post(self, sample_model):
+                db.session.add(sample_model)
+                db.session.commit()
+
+                return make_response(
+                    jsonify(mensagem="sample model saved successfully"), http.HTTPStatus.CREATED)
+    """
+
+
+class BootFlaskModel(BootFlaskFile):
+    pass
+
+
+class BootFlaskSchema(BootFlaskFile):
+    pass
+
+
+class BootFlaskDB(BootFlaskFile):
+    pass
+
+
+class BootFlaskApiInit(BootFlaskFile):
+    pass
